73787db34b1140111da8fd6056f4809c174abbb1
mariachernaya <5chernaya5.5@gmail.com>
2025-05-11 18:28:10 +0300

    Update auth.js

--- summer/auth.js
+++ summer/auth.js
@@ -10,13 +10,13 @@ $(document).ready(function() {
       $loginForm.hide();
       $registerForm.show();
       $title.text('Регистрация');
-      $registerForm.prepend('<p class="form-info">Уже есть аккаунт?</p>');
+      $loginForm.prepend('<p class="form-info">Нет аккаунта?</p>');
       $toggle.text('Войти');
     } else {
       $registerForm.hide();
       $loginForm.show();
       $title.text('Вход');
-      $loginForm.prepend('<p class="form-info">Нет аккаунта?</p>');
+      $registerForm.prepend('<p class="form-info">Уже есть аккаунт?</p>');
       $toggle.text('Зарегистрироваться');
     }
   });
